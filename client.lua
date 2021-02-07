2d31edf35f00b6c8effd146859f3a0052274c83d
xMqrcus <66357889+xMqrcus@users.noreply.github.com>
2021-02-07 19:25:01 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,89 @@
+vRPdv = {}
+Tunnel.bindInterface("xMqrcus_onduty",vRPdv)
+Proxy.addInterface("xMqrcus_onduty",vRPdv)
+PMserver = Tunnel.getInterface("xMqrcus_onduty","xMqrcus_onduty")
+vRPserver = Tunnel.getInterface("vRP","xMqrcus_onduty")
+vRP = Proxy.getInterface("vRP", "xMqrcus_onduty")
+
+local once = false
+
+Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(10000)
+	  if once == true then
+		  once = false
+		  TriggerServerEvent("xmq:jobafpå")
+	  else
+		once = false
+	  end
+	end
+end)
+
+
+RegisterNetEvent("onduty")
+AddEventHandler("onduty", function()
+  exports["rp-radio"]:GivePlayerAccessToFrequencies(1)
+  exports["rp-radio"]:GivePlayerAccessToFrequencies(2)
+  exports["rp-radio"]:GivePlayerAccessToFrequencies(3)
+  exports["rp-radio"]:GivePlayerAccessToFrequencies(4)
+  exports["rp-radio"]:GivePlayerAccessToFrequencies(5)
+end)
+    
+RegisterNetEvent("offduty")
+AddEventHandler("offduty", function()
+  exports["rp-radio"]:RemovePlayerAccessToFrequencies(1)
+  exports["rp-radio"]:RemovePlayerAccessToFrequencies(2)
+  exports["rp-radio"]:RemovePlayerAccessToFrequencies(3)
+  exports["rp-radio"]:RemovePlayerAccessToFrequencies(4)
+  exports["rp-radio"]:RemovePlayerAccessToFrequencies(5)
+	TriggerEvent("pNotify:SendNotification",{text = "Du har taget fri fra arbejde.",type = "success",timeout = (5000),layout = "BottomCenter",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+end)
+
+function DrawText3Ds(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+  end
+
+Citizen.CreateThread(function()
+    Citizen.Wait(1)
+    while true do
+      Citizen.Wait(1) 
+       -- POLITI
+		  if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), 440.21273803711,-975.62036132813,30.689321517944) < 3 then
+			  DrawMarker(20, 440.21273803711,-975.62036132813,30.689321517944-0.2, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5001, 83, 255, 87, 200, 1, 1, 0, 1)
+        DrawText3Ds(440.21273803711,-975.62036132813,30.689321517944+0.1, "~r~[F]~s~ For at gå af/på arbejde.")
+        if IsControlJustPressed(1, 145) then
+          TriggerServerEvent("xmq:jobafpå")
+        end
+
+        -- EMS
+      elseif GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), 267.29092407227,-1361.5251464844,24.537780761719) < 3 then
+			  DrawMarker(20, 267.29092407227,-1361.5251464844,24.537780761719-0.2, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5001, 83, 255, 87, 200, 1, 1, 0, 1)
+        DrawText3Ds(267.29092407227,-1361.5251464844,24.537780761719+0.1, "~r~[F]~s~ For at gå af/på arbejde.")
+        if IsControlJustPressed(1, 145) then
+          TriggerServerEvent("xmq:jobafpå")
+        end
+
+        -- MEKANIKER
+      elseif GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), 543.83715820313,-191.41227722168,54.481349945068) < 3 then
+        DrawMarker(20, 543.83715820313,-191.41227722168,54.481349945068-0.2, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5001, 83, 255, 87, 200, 1, 1, 0, 1)
+        DrawText3Ds(543.83715820313,-191.41227722168,54.481349945068+0.1, "~r~[F]~s~ For at gå af/på arbejde.")
+        if IsControlJustPressed(1, 145) then
+          TriggerServerEvent("xmq:jobafpå")
+        end
+      end
+    end
+end)
+
+
---
+++ server.lua
@@ -0,0 +1,42 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP", "xMqrcus_onduty")
+vRPclient = Tunnel.getInterface("vRP","xMqrcus_onduty")
+
+
+RegisterServerEvent('xmq:jobafpå')
+AddEventHandler('xmq:jobafpå', function()
+    local source = source
+	local user_id = vRP.getUserId({source})
+  -- POLITI
+	if vRP.hasGroup({user_id,"police"}) then
+		if vRP.hasGroup({user_id,"Politi-Job"}) then
+			TriggerClientEvent('offduty', source)
+			vRP.removeUserGroup({user_id, "Politi-Job"})
+		else
+			TriggerClientEvent('onduty', source)
+			vRP.addUserGroup({user_id, "Politi-Job"})
+		end
+
+  -- EMS
+	elseif vRP.hasGroup({user_id, "ems"}) then
+		if vRP.hasGroup({user_id,"EMS-Job"}) then
+			TriggerClientEvent('offduty', source)
+			vRP.removeUserGroup({user_id, "EMS-Job"})
+		else
+			TriggerClientEvent('onduty', source)
+			vRP.addUserGroup({user_id, "EMS-Job"})
+		end
+
+  -- Mekaniker
+	elseif vRP.hasGroup({user_id, "mechanic"}) then
+		if vRP.hasGroup({user_id,"Mekaniker-Job"}) then
+			TriggerClientEvent('offduty', source)
+			vRP.removeUserGroup({user_id, "Mekaniker-Job"})
+		else
+			TriggerClientEvent('onduty', source)
+			vRP.addUserGroup({user_id, "Mekaniker-Job"})
+		end
+	end
+end)
